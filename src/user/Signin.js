beffb6f813d275c3651659cac921c0fcddc40f05
Adarsh-thedev <adarshpittu@gmail.com>
2020-05-26 19:42:45 +0530

    signin

--- src/user/Signin.js
+++ src/user/Signin.js
@@ -3,9 +3,34 @@ import Base from '../core/Base';
 import {Link} from 'react-router-dom';
 
 const Signin = () => {
+    const signInForm = () => {
+        return(
+            <div className="container">
+                <div className="pa4 black-80 br1 shadow-3 bg1 col-12 col-md-6 offset-md-3">
+                    <form className="measure center">
+                        <div  className="ba b--transparent ph0 mh0">
+                            <div className="mt3">
+                                <label className="db fw6 lh-copy f6">Email</label>
+                                <input className="pa2 input-reset ba bg2 hover-bg-black hover-white w-100" type="email"/>
+                            </div>
+                            <div className="mv3">
+                                <label className="db fw6 lh-copy f6">Password</label>
+                                <input className="b pa2 input-reset ba bg2 hover-bg-black hover-white w-100" type="password"/>
+                            </div>
+                        </div>
+                        <div>
+                            <button className="btn btn-block bg-green grow pointer f6 br1">
+                                Submit
+                            </button>
+                        </div>
+                    </form>
+                </div>
+            </div>
+        );
+    }
     return(
         <Base title = 'Signin Page' description = 'User Signin'>
-            <h1>Working</h1>
+            {signInForm()}
         </Base>
     );
 }
