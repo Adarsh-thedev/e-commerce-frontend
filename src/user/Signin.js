eebe19215a188a0c0b41a27b928740f1746df9a9
Adarsh-thedev <adarshpittu@gmail.com>
2020-05-27 19:17:28 +0530

    signin page connected with DB

--- src/user/Signin.js
+++ src/user/Signin.js
@@ -1,8 +1,97 @@
 import React, {useState} from 'react';
 import Base from '../core/Base';
-import {Link} from 'react-router-dom';
+import {Link, Redirect} from 'react-router-dom';
+import {signin, authenticate, isAuthenticated} from '../auth/helper/index';
 
 const Signin = () => {
+    const [values, setValues] = useState({
+        email : '',
+        password : '',
+        error : '',
+        loading : false,
+        didRedirect : false
+    });
+
+    const {email, password, error, loading, didRedirect} = values;
+
+    const {user} = isAuthenticated;
+
+    const handleChange = name => event => {
+        setValues({...values, error : false, [name] : event.target.value});
+    }
+
+    const onSubmit = event => {
+        event.preventDefault();
+        setValues({
+            ...values, 
+            error : false,
+            loading : true
+        });
+        signin({email, password})
+        .then(data => {
+            if(data.error) {
+                setValues({
+                    ...values, 
+                    error : data.error,
+                    loading : false
+                }); 
+            } else {
+                authenticate(data, ()=> {
+                    setValues({
+                        ...values,
+                        email : '',
+                        password : '',
+                        error : false,
+                        didRedirect : false,
+                        didRedirect : true
+                    })
+                })
+            }
+        })
+        .catch(err => console.log('signin request failded'));
+    }
+
+    const performReDirect = () => {
+        //TODO: do a redirection instead of msg
+        if(didRedirect) {
+            if(user && user.role === '1') {
+                return <p>Redirect to admin</p>
+            } else{
+                return <p>Redirect to user dashboard </p>
+            }
+        }
+
+        if(isAuthenticated()) {
+            return <Redirect to = '/' />
+        }
+    };
+
+    const loadingMessage = () => {
+        return(
+            loading && (
+                <div className="alert alert-info">
+                    <h2>Loading...</h2>
+                </div>
+            )
+        );
+    }
+
+    const errorMessage = () => {
+        return(
+            <div className="row">
+                <div className="col-md-6 offset-sm-3 text-left">
+                    <div 
+                        className = 'alert alert-danger'
+                        style = {{display : error ? '' : 'none'}}
+                    >
+                        {error}
+                    </div>
+                </div>
+            </div>
+
+        );
+    }
+
     const signInForm = () => {
         return(
             <div className="container">
@@ -11,15 +100,25 @@ const Signin = () => {
                         <div  className="ba b--transparent ph0 mh0">
                             <div className="mt3">
                                 <label className="db fw6 lh-copy f6">Email</label>
-                                <input className="pa2 input-reset ba bg2 hover-bg-black hover-white w-100" type="email"/>
+                                <input 
+                                    className="pa2 input-reset ba bg2 hover-bg-black hover-white w-100" 
+                                    onChange = {handleChange('email')}
+                                    value = {email}
+                                    type="email"
+                                />
                             </div>
                             <div className="mv3">
                                 <label className="db fw6 lh-copy f6">Password</label>
-                                <input className="b pa2 input-reset ba bg2 hover-bg-black hover-white w-100" type="password"/>
+                                <input 
+                                    className="b pa2 input-reset ba bg2 hover-bg-black hover-white w-100" 
+                                    onChange = {handleChange('password')}
+                                    value = {password}
+                                    type="password"
+                                />
                             </div>
                         </div>
                         <div>
-                            <button className="btn btn-block bg1 grow pointer f6 br2">
+                            <button onClick = {onSubmit} className="btn btn-block bg1 grow pointer f6 br2">
                                 Submit
                             </button>
                         </div>
@@ -28,9 +127,14 @@ const Signin = () => {
             </div>
         );
     }
+
     return(
         <Base title = 'Signin Page' description = 'User Signin'>
+            {loadingMessage()}
+            {errorMessage()}
             {signInForm()}
+            {performReDirect()}
+            <p className = 'f6 text-center'>{JSON.stringify(values)}</p>
         </Base>
     );
 }
