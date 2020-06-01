d605a7283d8258e0b509f8eed66bd2947fa10e32
Adarsh-thedev <adarshpittu@gmail.com>
2020-06-01 17:15:00 +0530

    profile and dashboards according to role of user

---
+++ src/user/AdminDashBoard.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Base from '../core/Base';
+
+const AdminDashBoard = () => {
+    return(
+        <Base title = 'Admin DashBoard'>
+            <h1>Admin Dashboard</h1>
+        </Base>
+    );
+}
+
+export default AdminDashBoard;
