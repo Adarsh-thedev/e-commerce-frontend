0d91374ecdbf493c925c36a78b5c42eb52a453ef
Adarsh-thedev <adarshpittu@gmail.com>
2020-06-04 20:36:56 +0530

    passing product id to update the product

--- src/admin/ManageProducts.js
+++ src/admin/ManageProducts.js
@@ -65,7 +65,7 @@ const ManageProducts = () => {
                             <div className="col-4">
                             <Link
                                 className="btn btn-outline-success br2"
-                                to={`/admin/product/update/productId`}
+                                to={`/admin/product/update/${product._id}`}
                             >
                                 <span className="">Update</span>
                             </Link>
