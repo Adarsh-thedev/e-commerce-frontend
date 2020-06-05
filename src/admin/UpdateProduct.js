7b0e514b1926d65cd9ad6283eee06cc932ac9311
Adarsh-thedev <adarshpittu@gmail.com>
2020-06-05 10:27:37 +0530

    update product component, to be done later

---
+++ src/admin/UpdateProduct.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Base from '../core/Base';
+
+const UpdateProduct = () => {
+    return(
+        <Base>
+            Update Product Here(TODO)
+        </Base>
+    );
+}
+
+export default UpdateProduct;
