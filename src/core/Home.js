228808844200a3b38ec78a927676b4ffe8bdc6cc
Adarsh-thedev <adarshpittu@gmail.com>
2020-06-05 20:10:28 +0530

    showing cards on home page

--- src/core/Home.js
+++ src/core/Home.js
@@ -1,14 +1,26 @@
 import React from 'react';
 import {API} from '../Backend';
 import Base from './Base';
-//import 'tachyons';
+import Card from './Card';
 import '../styles.css';
 
 const Home = () => {
     console.log('API is ',API);
     return(
-        <Base title = 'Home Page'>
-            <h1>Home</h1>
+        <Base title="Home Page" description="Welcome to the Tshirt Store">
+            <div className = 'container'>
+                <div className="row">
+                    <div className="col-md-4 mb2">
+                        <Card/>
+                    </div>
+                    <div className="col-md-4 mb2">
+                        <Card/>
+                    </div>
+                    <div className="col-md-4 mb2">
+                        <Card/>
+                    </div>
+                </div>
+            </div>
         </Base>
     );
 }
