97eff93452198b6f771fb36346ba22e9c3caf764
Adarsh-thedev <adarshpittu@gmail.com>
2020-06-06 13:24:49 +0530

    All components getting loaded at homepage

--- src/core/Home.js
+++ src/core/Home.js
@@ -1,24 +1,45 @@
-import React from 'react';
-import {API} from '../Backend';
+import React, {useState, useEffect} from 'react';
 import Base from './Base';
 import Card from './Card';
 import '../styles.css';
+import {getProducts} from './helper/coreapicalls';
 
 const Home = () => {
-    console.log('API is ',API);
+    const [products, setProducts] = useState([]);
+    const [error, setError] = useState(false);
+
+    const preloadProducts = () => {
+        getProducts().then(data => {
+            if(data.error) {
+                setError(data.error);
+            } else{
+                setProducts(data);
+            }
+        })
+    }
+
+    const errorMessage = () => {
+        return error && (
+            <h1 className = 'red'>Currently out of servide...</h1>
+        );
+    }
+
+    useEffect(() => {
+        preloadProducts();
+    }, []);
+
     return(
         <Base title="Home Page" description="Welcome to the Tshirt Store">
             <div className = 'container'>
+                {errorMessage()}
                 <div className="row">
-                    <div className="col-md-4 mb2">
-                        <Card/>
-                    </div>
-                    <div className="col-md-4 mb2">
-                        <Card/>
-                    </div>
-                    <div className="col-md-4 mb2">
-                        <Card/>
-                    </div>
+                    {products.map((product, index) => {
+                        return(
+                            <div key = {index} className="col-md-4 mb2">
+                                <Card product = {product}/>
+                            </div>
+                        );
+                    })}
                 </div>
             </div>
         </Base>
