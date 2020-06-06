41107dfcfba5e2789c3c7d098845df61126d25d4
Adarsh-thedev <adarshpittu@gmail.com>
2020-06-06 13:23:23 +0530

    card for products

--- src/core/Card.js
+++ src/core/Card.js
@@ -22,13 +22,18 @@ const Card = ({
         );
     }
 
+    const cardTitle = product ? product.name : 'T-Shirt';
+    const cardPrice = product ? product.price : 'Default Price';
+    const cardDescription = product ? product.description : 'Default Description';
+
     return(
         <section>
             <div className = "card bn mw5 br2 bg-1 shadow-3 tc center grow">
-                <div className = 'card-header bg1 white'>T-Shirt</div>
+                <div className = 'card-header bg1 white'>{cardTitle}</div>
                 <ImageHelper product = {product}/>
                 <div className = "pa2 ma1 bt bn">
-                    <p className = "card-text db">Price: INR 599/-</p>
+                    <p className = "card-text bg1 white db">{cardDescription}</p>
+                    <p className = "card-text db">Price: {cardPrice}</p>
                     {shwoAddTtoCart()}
                     {showRemoveFromCart()}
                 </div>
