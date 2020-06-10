9162297a1b792e8924a61e80bc27abc51f51a6b2
Adarsh-thedev <adarshpittu@gmail.com>
2020-06-10 12:39:33 +0530

    new method to remove items from cart

--- src/core/helper/cartHelper.js
+++ src/core/helper/cartHelper.js
@@ -13,6 +13,22 @@ export const addItemToCart = (item, next) => {
     }
 }
 
+export const removeItemFromCart = (productId) => {
+    let cart = [];
+    if(typeof window !== undefined) {
+        if(localStorage.getItem('cart')) {
+            cart = JSON.parse(localStorage.getItem('cart'));
+        }
+        cart.forEach((product, index) => {
+            if(product._id === productId) {
+                cart.splice(index,1);
+            }
+        })
+        localStorage.setItem('cart', JSON.stringify(cart));
+    }
+    return cart;
+}
+
 export const loadCart = () => {
     if(typeof window !== undefined) {
         if(localStorage.getItem('cart')) {
