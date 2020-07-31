727da1cb12657da86e95e2e68b78368cb868e497
Adarsh-thedev <adarshpittu@gmail.com>
2020-07-31 21:15:51 +0530

    :hammer:

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
-import React from 'react';
-import Routes from './Routes';
-import ReactDom from 'react-dom';
-import 'tachyons';
+import React from "react";
+import Routes from "./Routes";
+import ReactDom from "react-dom";
+import "tachyons";
 
-ReactDom.render(<Routes/>, document.getElementById('root'));
+ReactDom.render(<Routes />, document.getElementById("root"));
